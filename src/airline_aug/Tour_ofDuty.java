32c63bdca253b54d563e808102103a59035c3aaa
zjcaledon <zjcaledon@localhost>
2019-09-03 19:34:05 +0800

    from little

---
+++ src/airline_aug/Tour_ofDuty.java
@@ -0,0 +1,17 @@
+package airline_aug;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Tour_ofDuty {
+	int ToDindex;
+	int settle;
+	String settleName;
+	int tSta, tEnd;
+	Date dateDep, dateArr;
+	ArrayList<ArrayList<Air_section>> arrDay = new ArrayList<>();
+
+	public Tour_ofDuty() {
+		
+	}
+}
