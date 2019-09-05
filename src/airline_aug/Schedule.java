c801de83b4437754197af853214d281f3b85443b
ToughJ <38240717+ToughJ@users.noreply.github.com>
2019-09-05 09:36:35 +0800

    Create Schedule.java

---
+++ src/airline_aug/Schedule.java
@@ -0,0 +1,14 @@
+package airline_aug;
+
+import java.util.ArrayList;
+
+public class Schedule {
+	ArrayList<Integer> acv = new ArrayList<>(), bcv = new ArrayList<>(), ccv = new ArrayList<>();
+	int city = 0;
+	int startDay = 0;
+	ArrayList<Tour_ofDuty> arrToD = new ArrayList<>();
+
+	public Schedule() {
+		
+	}
+}
